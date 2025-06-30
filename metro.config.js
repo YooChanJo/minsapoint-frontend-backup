49cc65a60eefac197c075e5fef450fa0389e92ed
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-07-01 00:24:47 +0900

    Shimmed react dom

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { withNativeWind } = require("nativewind/metro");
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
@@ -7,7 +8,15 @@ const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = mergeConfig(getDefaultConfig(__dirname), {});
+
+/* Resolving for shimmed react dom */
+const config = mergeConfig(getDefaultConfig(__dirname), {
+  resolver: {
+    extraNodeModules: {
+      "react-dom": path.resolve(__dirname, "shims/react-dom.js"),
+    },
+  },
+});
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
---
+++ shims/react-dom.js
@@ -0,0 +1,10 @@
+// do resolve react dom errors of gluestack ui v2
+
+
+// Empty shim for react-dom (used by @react-aria/utils in web contexts)
+module.exports = {
+  flushSync: () => {},
+};
+
+
+
