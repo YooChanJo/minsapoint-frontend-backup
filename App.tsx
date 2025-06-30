d4e584f786f1a59d0d393d5991e148bdd9bb6346
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-06-30 22:09:46 +0900

    Added gluestack

--- App.tsx
+++ App.tsx
@@ -1,27 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from "@react-native/new-app-screen";
-import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
+import { useState } from "react";
+// import "./android";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { Button, StyleSheet, Text, useColorScheme, View } from "react-native";
 
 function App() {
   const isDarkMode = useColorScheme() === "dark";
+  isDarkMode;
+  const [displayText, setDisplayText] = useState("");
+  const [flag, setFlag] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
+        <Button
+          title="any button"
+          onPress={() => {
+            flag ? setDisplayText("바보") : setDisplayText("최먼지는?");
+            setFlag(curr => !curr);
+          }}
+        >
+          <Text>Click me</Text>
+        </Button>
+        <Text style={styles.text}>{displayText}</Text>
+      </View>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "column",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  text: {
+    justifyContent: "center",
+    alignContent: "center",
+    fontSize: 40,
   },
 });
 
