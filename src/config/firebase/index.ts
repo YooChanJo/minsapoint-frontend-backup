2b9d53f5d01d90069f471d9e3cd66e931c7f69c3
Yoo-Chan Jo <jo.yoochani@gmail.com>
2025-08-02 01:39:36 +0900

    Removed firebase.json

--- src/config/firebase/index.ts
+++ src/config/firebase/index.ts
@@ -2,6 +2,16 @@
 import { getApp } from '@react-native-firebase/app';
 
 /* firebase.json configured for multithreading(?) in android environments */
+/* 
+{
+  "react-native": {
+    "android_task_executor_maximum_pool_size": 4,
+    "android_task_executor_keep_alive_seconds": 5
+  }
+}
+
+add this when deploy
+*/
 
 const firebaseApp = getApp();
 
